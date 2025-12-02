82d78383945b725588bc1f6e98e80937a4cefaa3
aoede <aoedeltd@gmail.com>
2025-12-02 21:31:39 +0000

    feat: build + production static assets

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,18 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
 import { createServer } from "@taujs/server";
 
 import config from "../taujs.config.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const clientRoot = path.resolve(__dirname, "../client");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const clientRoot = isDev
+  ? path.resolve(__dirname, "../client") // source
+  : path.resolve(__dirname, "../dist/client"); // build
 
 const startServer = async () => {
   try {
@@ -17,9 +23,16 @@ const startServer = async () => {
       fastify,
       config,
       clientRoot,
-      debug: {
-        ssr: process.env.NODE_ENV === "development",
-      },
+      staticAssets: isDev
+        ? false
+        : {
+            plugin: fastifyStatic,
+            options: {
+              root: clientRoot,
+              prefix: "/", // required: Vite asset URLs start with /
+            },
+          },
+      debug: { ssr: isDev },
     });
 
     await fastify.listen({
